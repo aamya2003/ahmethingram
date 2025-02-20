89e49ba698da1e3139dc096abef48b18ce7563cf
aamya2003 <ahmadasaadmajed1@gmail.com>
2025-02-20 23:20:34 +0300

    Initial commit

--- build/lib/ahmethingra/__init__.py
+++
@@ -1,8 +0,0 @@
-def say_hello(name):
-    return f"Hello, {name}!"
-
-def add(a, b):
-    return a + b
-def say(yi):
-    print(yi)
-    
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ahmethingra",  # اسم المكتبة
-    version="0.1.1",  # رقم الإصدار
+    name="ahmethingram",  # اسم المكتبة
+    version="0.0.1", 
     packages=find_packages(),
     install_requires=[],  # ضع المتطلبات هنا إن وجدت
     author="Ahmed Asaad",
     author_email="ahmadasaadmajed1@gmail.com",
     description="وصف مكتبتك",
-    url="https://pypi.org/project/ahmethingra/",
+    url="https://pypi.org/project/ahmethingram/",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
