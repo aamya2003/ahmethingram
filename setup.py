ee180031bd60460ea0da03da6cbf283bf19d41ad
aamya2003 <ahmadasaadmajed1@gmail.com>
2025-02-20 23:35:33 +0300

    4nd commit

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="ahmethingram",  # اسم المكتبة
-    version="0.0.3", 
+    version="0.0.4", 
     packages=find_packages(),
     install_requires=[],  # ضع المتطلبات هنا إن وجدت
     author="Ahmed Asaad",
     author_email="ahmadasaadmajed1@gmail.com",
-    description='Short description of your package',
+    description='😁🤩🤗🤗🤗🙂😚😁',
     long_description=open('README.md').read(),  # إذا كنت تستخدم ملف README.md للوصف
     long_description_content_type='text/markdown',  # استخدم هذا إذا كنت تستخدم Markdown
     url="https://pypi.org/project/ahmethingram/",
